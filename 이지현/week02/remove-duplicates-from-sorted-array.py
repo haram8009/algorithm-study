646b2db51d3b49090d98bf879e07c018aab8d338
jihyeon <wlgus7464@gmail.com>
2026-08-10 21:33:07 +0900

    [week02] Remove Duplicates from Sorted Array

---
+++ 이지현/week02/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        k = 0
+        for i in range(1, len(nums)):
+            if nums[k] != nums[i]:
+                k+=1
+                nums[k]= nums[i]
+
+        return k + 1
